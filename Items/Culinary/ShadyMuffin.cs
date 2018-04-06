724d6df58d25db32a14ac2e8db9962138f7e1927
CookieBiscuit <aidan.n.borne@gmail.com>
2018-04-05 21:36:32 -0500

    Create ShadyMuffin.cs

---
+++ Items/Culinary/ShadyMuffin.cs
@@ -0,0 +1,52 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CookieMod.Items.Culinary
+{
+	public class ShadyMuffin : CookClass
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Shady Muffin");
+			Tooltip.SetDefault("Gives food poisoning, <right> to eat!");
+		}
+		public override void SafeSetDefaults()
+		{
+			item.width = 16;
+			item.height = 16;
+      item.damage = 16;
+      item.crit = 6;
+      item.UseSound = SoundID.Item2;
+			item.maxStack = 30;
+			item.rare = 1;
+			item.consumable = true;
+      		item.value = 900;
+			item.shoot = mod.ProjectileType ("ShadyMuffin");
+			item.ammo = mod.ItemType("Cookie");
+		}
+		public override bool CanRightClick()
+		{
+			return true;
+		}
+
+		public override void RightClick(Player player)
+		{
+			player.AddBuff(mod.BuffType("FoodPoisoning"), 5400);
+		}
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null, "DoughBall", 2);
+			recipe.AddIngredient(ItemID.TissueSample, 3);
+			recipe.AddTile(TileID.Furnaces);			
+			recipe.SetResult(this, 2);
+			recipe.AddRecipe();
+		}
+	}
+}
