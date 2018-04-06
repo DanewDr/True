ad1afd9700d5b0b0bfc2e2c6835e32489113de62
CookieBiscuit <aidan.n.borne@gmail.com>
2018-04-05 19:01:41 -0500

    Update ThrowingCookieHoney.cs

--- Projectiles/ThrowingCookieHoney.cs
+++ Projectiles/ThrowingCookieHoney.cs
@@ -36,12 +36,6 @@ namespace CookieMod.Projectiles
 			{
 				Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, mod.DustType("HoneyCrumbs"), projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
 			}				           
-            projectile.ai[0] += 1f;
-            if (projectile.ai[0] >= 50f)       //how much time the projectile can travel before landing
-          	{
-				projectile.velocity.Y = projectile.velocity.Y + 0.05f;    // projectile fall velocity
-                projectile.velocity.X = projectile.velocity.X * 1f;    // projectile velocity
-            }
 		}
 		public override void Kill(int timeLeft)
 		{
