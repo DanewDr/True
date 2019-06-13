c4afa8bf31a5b6b1fdb8ca3003afee0cee4d3c40
DanewDr <danen1701@gmail.com>
2019-06-13 21:13:24 +1000

    Update ShadyMuffin.cs

--- Items/Culinary/ShadyMuffin.cs
+++ Items/Culinary/ShadyMuffin.cs
@@ -1,11 +1,12 @@
-
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Terraria;
-using Terraria.ID;
-using Terraria.ModLoader;
 
 namespace CookieMod.Items.Culinary
 {
